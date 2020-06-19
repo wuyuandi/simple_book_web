6eda137c9edff2332673ee8d9b52227643f4e9e5
wuyuandi <yuandi@ualberta.ca>
2020-06-19 12:50:01 -0600

    404page-2

--- src/pages/nomatch/index.js
+++ src/pages/nomatch/index.js
@@ -1,19 +1,29 @@
 /*
  * @Author: your name
  * @Date: 2020-06-17 17:06:16
- * @LastEditTime: 2020-06-17 18:59:35
+ * @LastEditTime: 2020-06-19 10:30:16
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /simple_book_web/src/pages/nomatch/index.js
  */ 
 import React from 'react'
+import './404.css';
+import styled from "styled-components";
+const NotFound = styled.div `
+    width: 100%;
+    height:1200px;
+    position:relative;
+    display: grid;
+`
 
 export default function NoFound() {
     return (
-        
-        <div>
-            404 NOT FOUND
+        <NotFound >
+        <div className="WaterBallWrapper">
+            <div className="WaterBallWave">
+            </div>
         </div>
+        </NotFound>
     )
 
 }
